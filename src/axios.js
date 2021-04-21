89378e4a5afd797488430fbf0c466b4f41df1ded
Praveen Matoria <praveen.matoria@gmail.com>
2021-04-22 00:51:55 +0530

    BASE URL fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL_API,
+  //baseURL: "http://localhost:5001/clone-a7dd5/us-central1/api", // API (cloud function) URL
+  baseURL: "https://us-central1-clone-a7dd5.cloudfunctions.net/api", // API (cloud function) URL
 });
 
 export default instance;
